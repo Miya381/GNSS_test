cecff330569af5cce509ce5532daf46812b99286
NishiNobu <32629924+NishiNobu@users.noreply.github.com>
2017-11-15 12:55:26 +0900

    Rejected commit: Master

--- app/src/main/java/com/kubolab/gnss/gnssloggerR/SettingsFragment.java
+++ app/src/main/java/com/kubolab/gnss/gnssloggerR/SettingsFragment.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.kubolab.gnss.gnssloggerR;
 
 import android.app.Activity;
@@ -66,7 +50,6 @@ public class SettingsFragment extends Fragment {
     private FileLogger mFileLogger;
     private UiLogger mUiLogger;
     private GnssContainer mGnssContainer;
-    private HelpDialog helpDialog;
     private TextView EditSaveLocation;
     public static boolean FIRST_CHECK = false;
 
